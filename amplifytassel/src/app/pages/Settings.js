c1e56da5f7388602aed1f898f442a1615477d378
Abdullah Kose <akose@ucsc.edu>
2023-05-09 13:00:11 -0700

    add react-spring package

--- amplifytassel/src/app/pages/Settings.js
+++ amplifytassel/src/app/pages/Settings.js
@@ -4,8 +4,23 @@ import useAuth from '../util/AuthContext';
 import { DataStore } from '@aws-amplify/datastore';
 import { Keyword, KeywordProfile, Profile } from '../../models';
 
+// Animation Libraries
 import Confetti from 'react-confetti'
 import useWindowSize from "../util/useWindowSize"
+import { animated, useSpring, useSprings } from '@react-spring/web'
+import StarRoundedIcon from '@mui/icons-material/StarRounded';
+
+// Example animation from react-spring
+const StarFalling = ({ isVisible, children }) => {
+  const styles = useSpring({
+    position: "absolute",
+    opacity: isVisible ? 1 : 0,
+    y: isVisible ? 0 : 240,
+    x: isVisible ? 240 : 0,
+  })
+
+  return <animated.div style={styles}>{children}</animated.div>
+}
 
 /**
  * creates settings page
@@ -15,6 +30,7 @@ export default function Settings() {
   const {user, userProfile, setUserProfile} = useAuth();
   const { width, height } = useWindowSize();
   const [showConfetti, setShowConfetti] = useState(false);
+  const [fadeVisible, setFadeVisible] = useState(false)
   
   const testQuery = () => {
     let profileIds = ['2cda8741-543a-4614-83cc-c28fae98adf2', '03bc5db5-46b3-4100-942b-e9ddb89ec1cc'];
@@ -30,7 +46,14 @@ export default function Settings() {
   return (
     <div className='Settings'>
       <h1>Settings</h1>
+      <StarFalling isVisible={fadeVisible}>
+        <StarRoundedIcon
+          className='icon-yellow'
+          sx={{mr: 3, transform: 'scale(2)'}}
+        />
+      </StarFalling>
       <button onClick={testQuery}>test</button>
+      <button onClick={() => {setFadeVisible(!fadeVisible)}}>Star Button</button>
       <button onClick={() => {
         setShowConfetti(true); 
         // setTimeout(() => {setShowConfetti(false)}, 3000);
