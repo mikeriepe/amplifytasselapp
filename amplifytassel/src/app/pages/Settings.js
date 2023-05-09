3b913d92eae4eee3633651b536db7dc9c5e94ee3
Abdullah Kose <akose@ucsc.edu>
2023-05-08 20:49:10 -0700

    add confetti animation on settings page

--- amplifytassel/src/app/pages/Settings.js
+++ amplifytassel/src/app/pages/Settings.js
@@ -1,8 +1,11 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import useAuth from '../util/AuthContext';
 
 import { DataStore } from '@aws-amplify/datastore';
 import { Keyword, KeywordProfile, Profile } from '../../models';
+
+import Confetti from 'react-confetti'
+import useWindowSize from "../util/useWindowSize"
 
 /**
  * creates settings page
@@ -10,6 +13,8 @@ import { Keyword, KeywordProfile, Profile } from '../../models';
  */
 export default function Settings() {
   const {user, userProfile, setUserProfile} = useAuth();
+  const { width, height } = useWindowSize();
+  const [showConfetti, setShowConfetti] = useState(false);
   
   const testQuery = () => {
     let profileIds = ['2cda8741-543a-4614-83cc-c28fae98adf2', '03bc5db5-46b3-4100-942b-e9ddb89ec1cc'];
@@ -21,11 +26,26 @@ export default function Settings() {
         console.log(err);
       });
   };
-
+  
   return (
     <div className='Settings'>
       <h1>Settings</h1>
       <button onClick={testQuery}>test</button>
+      <button onClick={() => {
+        setShowConfetti(true); 
+        // setTimeout(() => {setShowConfetti(false)}, 3000);
+        // setTimeout(setShowConfetti(false), 3000);
+      }}>Confetti</button>
+      {showConfetti &&
+        <Confetti
+        width={width}
+        height={height}
+        recycle={false}
+        numberOfPieces={2000}
+        tweenDuration={10000}
+        onConfettiComplete={() => {setShowConfetti(false)}}
+        />
+      }
     </div>
   );
 }
