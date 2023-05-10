775db7d0d538911339ad7b239912cdc2fa889ce0
Abdullah Kose <akose@ucsc.edu>
2023-05-10 16:42:41 -0700

    Add animation star and confetti animation components

--- amplifytassel/src/app/pages/Settings.js
+++ amplifytassel/src/app/pages/Settings.js
@@ -1,47 +1,18 @@
-import React, {useState, useRef, useEffect} from 'react';
-import useAuth from '../util/AuthContext';
+import React, { useState } from 'react';
 
 import { DataStore } from '@aws-amplify/datastore';
-import { Keyword, KeywordProfile, Profile } from '../../models';
+import { KeywordProfile } from '../../models';
 
 // Animation Libraries
-import Confetti from 'react-confetti'
-import useWindowSize from "../util/useWindowSize"
-import { animated, useSpring, useSprings, useSpringRef, useChain, useTransition, useTrail } from '@react-spring/web'
-import StarRoundedIcon from '@mui/icons-material/StarRounded';
-
-const StarFalling = ({ windowWidth, windowHeight, hideAnimation, children }) => {
-  const firstStep = useSpring({
-    from: { position: "absolute", opacity: 0, x: (windowWidth/windowHeight) * 200, y: windowHeight - 200},
-    to: [
-      { opacity: 1, x: (windowWidth/windowHeight) * 500, y: windowHeight - 500},
-      { opacity: 0, x: windowWidth - (windowWidth/windowHeight) * 200, y: 200, onRest: hideAnimation},
-    ],
-    config: {
-      friction: 40,
-    },
-  })
-
-  return (
-    <animated.div style={firstStep}>
-      {children}
-    </animated.div>
-  )
-}
-
+import AnimationStarFlying from '../components/AnimationStarFlying';
+import AnimationConfetti from '../components/AnimationConfetti';
 /**
  * creates settings page
  * @return {HTML} settings page
  */
 export default function Settings() {
-  const {user, userProfile, setUserProfile} = useAuth();
-  const { width, height } = useWindowSize();
   const [showConfetti, setShowConfetti] = useState(false);
-  const [fadeVisible, setFadeVisible] = useState(false)
-
-  const hideAnimation = () => {
-    setFadeVisible(false);
-  };
+  const [showStar, setShowStar] = useState(false)
   
   const testQuery = () => {
     let profileIds = ['2cda8741-543a-4614-83cc-c28fae98adf2', '03bc5db5-46b3-4100-942b-e9ddb89ec1cc'];
@@ -57,30 +28,14 @@ export default function Settings() {
   return (
     <div className='Settings'>
       <h1>Settings</h1>
-      {fadeVisible &&
-        <StarFalling windowWidth={width} windowHeight={height} hideAnimation={hideAnimation}>
-          <StarRoundedIcon
-            className='icon-yellow'
-            sx={{mr: 3, transform: 'scale(3)'}}
-          />
-        </StarFalling>
-      }
       <button onClick={testQuery}>test</button>
-      <button onClick={() => {setFadeVisible(!fadeVisible)}}>Star Button</button>
-      <button onClick={() => {
-        setShowConfetti(true); 
-        // setTimeout(() => {setShowConfetti(false)}, 3000);
-        // setTimeout(setShowConfetti(false), 3000);
-      }}>Confetti</button>
+      <button onClick={() => {setShowStar(true)}}>Star Button</button>
+      {showStar &&
+        <AnimationStarFlying setVisible={setShowStar} />
+      }
+      <button onClick={() => {setShowConfetti(true)}}>Confetti</button>
       {showConfetti &&
-        <Confetti
-        width={width}
-        height={height}
-        recycle={false}
-        numberOfPieces={2000}
-        tweenDuration={10000}
-        onConfettiComplete={() => {setShowConfetti(false)}}
-        />
+        <AnimationConfetti setVisible={setShowConfetti}/>
       }
     </div>
   );
