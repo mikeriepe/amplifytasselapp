82534c5db38efc6a79d7bd7d92f42352f12730d0
Collin <67039203+CSWXM@users.noreply.github.com>
2023-05-26 14:39:50 -0700

    Added testing for opportunities page (#42)
    Co-authored-by: Vlad L <102999933+ulekhtsi@users.noreply.github.com>

--- amplifytassel/src/app/components/CompressedTabBar.js
+++ amplifytassel/src/app/components/CompressedTabBar.js
@@ -38,6 +38,7 @@ export default function CompressedTabBar({data, tab, setTab, type}) {
       }}
     >
       <Tabs
+        aria-label='Tabs'
         value={tab}
         onChange={handleTabs}
         indicatorColor='primary'
@@ -54,6 +55,7 @@ export default function CompressedTabBar({data, tab, setTab, type}) {
           <Tab
             aria-label={`Opportunities Tab ${object.name}`}
             key={`tab-id-${Math.random()}`}
+            data-test-id={object.name}
             label={object.name}
             sx={TabStyles}
             disableRipple
