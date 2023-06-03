25e195c7522475ff24539174ea59b22cb75c8fbe
roalbarr <roalbarr@ucsc.edu>
2023-06-02 17:02:12 -0700

    request creation tests

--- amplifytassel/cypress/e2e/ViewOppRequestToJoin.cy.js
+++ amplifytassel/cypress/e2e/ViewOppRequestToJoin.cy.js
@@ -11,60 +11,65 @@ beforeEach(function () {
 
 describe('Request to join as a General Participant', () => {
     it('Requests to join', () => {
-        cy.visit('http://localhost:3000/Opportunity/91e4f69c-2aba-417d-94e0-db7546f4b923');
+        cy.visit('http://localhost:3000/Opportunity/5ca704c0-1d6f-4624-a2e2-5ca7207ba21e');
         cy.wait(2000);
         cy.get(selectors.ViewOpportunityRequest).contains('Request to Join').click();
         cy.get(selectors.RequestModalSendRequest).contains('Send Request to Join').click();
-        cy.wait(4000);
+        cy.wait(1000);
         cy.visit('http://localhost:3000/opportunities');
         cy.wait(2000);
         cy.get(selectors.OpportunitiesTabPending).click();
         cy.wait(1000);
-        cy.get(selectors.OpportunityCardTitle1).contains('Test Dummies Become Human');
-        cy.get(selectors.OpportunityCardHost1).contains('Abdullah Kose');
+        cy.get(selectors.OpportunityCardTitle6).contains('Banana Throwing Party');
+        cy.get(selectors.OpportunityCardHost6).contains('Jimmy Neutron');
         cy.wait(1000);
     });
     it('Cancels the request', () => {
         // Click on the pending Tab
+        cy.visit('http://localhost:3000/opportunities');
+        cy.wait(2000);
         cy.get(selectors.OpportunitiesTabPending).click();
-        cy.wait(1000);
+        //cy.scrollTo('bottom');
         // Click on delete button
         cy.get(selectors.DeleteRequests).click();
         cy.wait(1000);
         cy.get(selectors.OpportunitiesTabBrowse).click();
         cy.wait(1000);
-        cy.get(selectors.OpportunityCardTitle1).contains('Test Dummies Become Human');
-        cy.get(selectors.OpportunityCardHost1).contains('Abdullah Kose');
+        cy.get(selectors.OpportunityCardTitle6).contains('Banana Throwing Party');
+        cy.get(selectors.OpportunityCardHost6).contains('Jimmy Neutron');
         cy.wait(1000);
       });
 });
 
 describe('Request to join a Specific Role', () => {
     it('Requests to join', () => {
-        cy.visit('http://localhost:3000/Opportunity/91e4f69c-2aba-417d-94e0-db7546f4b923');
+        cy.visit('http://localhost:3000/Opportunity/5ca704c0-1d6f-4624-a2e2-5ca7207ba21e');
         cy.wait(2000);
-        cy.get(selectors.RoleRequest).contains('Request Specific Role').click();
+        cy.scrollTo('bottom');
+        cy.get(selectors.RoleRequest).click();
         cy.get(selectors.RequestModalSendRequest).contains('Send Request to Join').click();
-        cy.wait(4000);
+        cy.wait(1000);
         cy.visit('http://localhost:3000/opportunities');
         cy.wait(2000);
         cy.get(selectors.OpportunitiesTabPending).click();
         cy.wait(1000);
-        cy.get(selectors.OpportunityCardTitle1).contains('Test Dummies Become Human');
-        cy.get(selectors.OpportunityCardHost1).contains('Abdullah Kose');
+        cy.get(selectors.OpportunityCardTitle6).contains('Banana Throwing Party');
+        cy.get(selectors.OpportunityCardHost6).contains('Jimmy Neutron');
         cy.wait(1000);
     });
     it('Cancels the request', () => {
         // Click on the pending Tab
+        cy.visit('http://localhost:3000/opportunities');
+        cy.wait(2000);
         cy.get(selectors.OpportunitiesTabPending).click();
-        cy.wait(1000);
+        cy.scrollTo('bottom');
         // Click on delete button
         cy.get(selectors.DeleteRequests).click();
         cy.wait(1000);
         cy.get(selectors.OpportunitiesTabBrowse).click();
         cy.wait(1000);
-        cy.get(selectors.OpportunityCardTitle1).contains('Test Dummies Become Human');
-        cy.get(selectors.OpportunityCardHost1).contains('Abdullah Kose');
+        cy.get(selectors.OpportunityCardTitle6).contains('Banana Throwing Party');
+        cy.get(selectors.OpportunityCardHost6).contains('Jimmy Neutron');
         cy.wait(1000);
       });
 });
