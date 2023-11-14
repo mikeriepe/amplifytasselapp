ea7ac2e8cc54693d6b4221348c7a198d3234e53d
Darren Lin <dlin52@ucsc.edu>
2023-11-14 15:50:59 -0800

    fixed naming

--- amplifytassel/src/app/components/SocialIncomingRequests.js
+++ amplifytassel/src/app/components/SocialIncomingRequests.js
@@ -183,7 +183,7 @@ export default function SocialIncomingRequests() {
   const handleSelectAll = () => {
     setSelectAllChecked(!selectAllChecked);
     if (!selectAllChecked) {
-      const newSelected = displayUsers.map((user) => user.email);
+      const newSelected = displayFriendRequests.map((user) => user.email);
       console.log(newSelected);
       setSelected(newSelected);
     } else {
