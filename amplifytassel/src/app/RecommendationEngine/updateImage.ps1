f0c1293a53b23aa516698ef184c43bc596ac0bbb
Giridhar Vadhul <gvadhul@ucsc.edu>
2024-04-24 14:55:44 -0700

    Updated updateImage to allow for either image - recommendation-engine or reverse-recommendation-engine

--- amplifytassel/src/app/RecommendationEngine/updateImage.ps1
+++ amplifytassel/src/app/RecommendationEngine/updateImage.ps1
@@ -1,9 +1,9 @@
-docker build -t recommendation-engine:1.4 .
+docker build -t $args[1]:latest .
 
 # aws ecr create-repository --repository-name recommendation-engine --image-scanning-configuration scanOnPush=true --region us-west-1
 
-docker tag recommendation-engine:1.4 $args[0].dkr.ecr.us-west-1.amazonaws.com/recommendation-engine:latest
+docker tag $args[1]:latest $args[0].dkr.ecr.us-west-1.amazonaws.com/$args[1]:latest
 
 aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin $args[0].dkr.ecr.us-west-1.amazonaws.com
 
-docker push $args[0].dkr.ecr.us-west-1.amazonaws.com/recommendation-engine:latest
+docker push $args[0].dkr.ecr.us-west-1.amazonaws.com/$args[1]:latest
